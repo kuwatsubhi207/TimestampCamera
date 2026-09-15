91bfc7079f025f84b14d05a1d111ccf4ae2f3977
subhi78 <kuwatsubhi207@gmail.com>
2026-09-15 17:02:59 +0700

    update veersi 1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 25
         targetSdk = 37
         versionCode = 1
-        versionName = "1.2"
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
